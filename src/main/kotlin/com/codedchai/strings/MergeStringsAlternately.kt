1d78021d6e60b912bdb53a2b22d066e73caca14c
Connor Hasting <guardianx515@gmail.com>
2024-02-01 20:54:43 -0600

    Merge strings alternately but with less code

--- src/main/kotlin/com/codedchai/strings/MergeStringsAlternately.kt
+++ src/main/kotlin/com/codedchai/strings/MergeStringsAlternately.kt
@@ -1,26 +1,21 @@
 package com.codedchai.strings
 
+import kotlin.math.max
+
 fun main() {
-  mergeAlternately("abc", "pqr")
+  mergeAlternately("abc", "pqrs")
     .also { println(it) }
 }
 
 fun mergeAlternately(word1: String, word2: String): String {
-  var word1Index = 0
-  var word2Index = 0
   val sb = StringBuilder()
 
-  while (word1Index < word1.length || word2Index < word2.length) {
-
-    if ((word1Index + word2Index) % 2 == 0 && word1Index < word1.length) {
-      sb.append(word1[word1Index])
-      word1Index++
-    } else if (word2Index < word2.length) {
-      sb.append(word2[word2Index])
-      word2Index++
-    } else {
-      sb.append(word1[word1Index])
-      word1Index++
+  (0 until max(word1.length, word2.length)).forEach { idx ->
+    if (idx < word1.length) {
+      word1[idx].also { sb.append(it) }
+    }
+    if (idx < word2.length) {
+      word2[idx].also { sb.append(it) }
     }
   }
 
