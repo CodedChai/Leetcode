b49a7c7c4f07ef131d006bc66a63063df10c48e2
Connor Hasting <guardianx515@gmail.com>
2024-02-03 13:26:35 -0600

    it's still really slow but it passes, will check out how to optimize it

--- src/main/kotlin/com/codedchai/windows/MaxAverageSubarrayOne.kt
+++ src/main/kotlin/com/codedchai/windows/MaxAverageSubarrayOne.kt
@@ -1,10 +1,22 @@
 package com.codedchai.windows
 
+import kotlin.math.max
+
 fun main() {
-  findMaxAverage(listOf(1, 2, 3).toIntArray(), 2)
+  findMaxAverage(listOf(0, 1, 1, 3, 3).toIntArray(), 4)
     .also { println(it) }
 }
 
 fun findMaxAverage(nums: IntArray, k: Int): Double {
-  return nums.asSequence().windowed(k, 1).maxOf { it.sum() } / k.toDouble()
+  var currentMax = nums.take(k).sum()
+
+  for (i in 1..nums.size - k) {
+    var windowSum = 0
+    for (j in i until i + k) {
+      windowSum += nums[j]
+    }
+    currentMax = max(currentMax, windowSum)
+  }
+
+  return currentMax.toDouble() / k.toDouble()
 }
