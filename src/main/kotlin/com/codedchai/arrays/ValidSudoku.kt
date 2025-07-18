3e623671779271809271097dc60852000d4b76a5
codedchai <guardianx515@gmail.com>
2025-07-18 15:01:33 -0500

    Is valid sudoku optimal

--- src/main/kotlin/com/codedchai/arrays/ValidSudoku.kt
+++ src/main/kotlin/com/codedchai/arrays/ValidSudoku.kt
@@ -2,6 +2,33 @@ package com.codedchai.arrays
 
 class ValidSudoku {
     fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val rows = Array(9) { BooleanArray(10) }
+        val columns = Array(9) { BooleanArray(10) }
+        val boxes = Array(9) { BooleanArray(10) }
+
+        for (i in 0 until 9) {
+            for (j in 0 until 9) {
+                val char = board[i][j]
+                if (char == '.') continue
+
+                val digit = char.digitToInt()
+                val boxIndex = (i / 3) * 3 + j / 3
+
+                if (rows[i][digit] || columns[j][digit] || boxes[boxIndex][digit]) {
+                    return false
+                }
+
+                rows[i][digit] = true
+                columns[j][digit] = true
+                boxes[boxIndex][digit] = true
+
+            }
+        }
+
+        return true
+    }
+
+    fun isValidSudokuIntuition(board: Array<CharArray>): Boolean {
         val isValidRows = board.all { row -> row.isValid() }
         val columns = (0 until board.size).map { columnIndex ->
             board.map { row -> row[columnIndex] }.toCharArray()
