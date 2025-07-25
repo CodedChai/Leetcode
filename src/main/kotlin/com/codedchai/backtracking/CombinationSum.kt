f9b9911b176c5453682b4208aef4d7dcb0bf36e3
codedchai <guardianx515@gmail.com>
2025-07-25 11:32:34 -0500

    CombinationSum

--- src/main/kotlin/com/codedchai/backtracking/CombinationSum.kt
+++ src/main/kotlin/com/codedchai/backtracking/CombinationSum.kt
@@ -3,6 +3,26 @@ package com.codedchai.backtracking
 class CombinationSum {
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
 
-        TODO()
+        val results = mutableListOf<List<Int>>()
+        val currentList = mutableListOf<Int>()
+
+        fun backtrack(currentIndex: Int, currentTarget: Int) {
+            if (currentTarget < 0 || currentIndex >= candidates.size || currentList.size > 150) {
+                return
+            }
+            if (currentTarget == 0) {
+                results.add(currentList.toList())
+                return
+            }
+
+            currentList.add(candidates[currentIndex])
+            backtrack(currentIndex, currentTarget - candidates[currentIndex])
+
+            currentList.removeLast()
+            backtrack(currentIndex + 1, currentTarget)
+        }
+
+        backtrack(0, target)
+        return results
     }
 }
